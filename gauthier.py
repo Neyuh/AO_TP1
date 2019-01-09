c2d27c88a3b6feb6c2922985c15621a1451c8248
Neyuh <36778001+Neyuh@users.noreply.github.com>
2019-01-09 08:42:33 +0100

    update gauthier.py
    inversion de signes

--- gauthier.py
+++ gauthier.py
@@ -1,18 +1,18 @@
-
 def update_iterations_count(change,iterations_count):
-  if change==True:
+  if change==False:
     iterations_count+=1
-    print(iterations_count)
+    #print(iterations_count)
   else:
     iterations_count=0
-    print(iterations_count)
+    #print(iterations_count)
   return iterations_count
+
 
 def stop_condition(iterations_count,limite):
   if iterations_count > limite:
     return 0
   else:
-    return 1   
+    return 1  
 
 if __name__== '__main__':
   import random
